19e3ff354a3d6c3786bc7e195709fbd65c32ba71
japoncio3k <gustavo.kamihara@gmail.com>
2022-09-06 13:26:40 -0300

    Refs are now const and separated javascript from jsx

--- app.tsx
+++ app.tsx
@@ -16,11 +16,25 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const [error, setError] = React.useState('');
-  let email = React.useRef('').current;
-  let password = React.useRef('').current;
+  const email = React.useRef('');
+  const password = React.useRef('');
   const emailRegex = new RegExp('[a-zA-Z.]+@(?:[a-zA-Z]+)+.com');
   const passNumberRegex = new RegExp('[0-9]');
   const passCharRegex = new RegExp('[A-z]');
+
+  const handleButtonPress = () => {
+    if (email.current == '' || password.current == '') {
+      setError('Preencha todos os campos');
+    } else if (password.current.length < 7) {
+      setError('A senha deve possuir pelo menos 7 caracteres');
+    } else if (!emailRegex.test(email.current)) {
+      setError('O campo de email deve seguir o formato de um email: abc@def.com');
+    } else if (!passNumberRegex.test(password.current) || !passCharRegex.test(password.current)) {
+      setError('O campo de senha deve conter ao menos uma letra e um número');
+    } else {
+      setError('');
+    }
+  };
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.white,
@@ -32,7 +46,7 @@ const App = () => {
         <Text>E-mail</Text>
         <TextInput
           onChangeText={(text) => {
-            email = text;
+            email.current = text;
           }}
           placeholder='ex:joao.silva@gmail.com'
         />
@@ -43,27 +57,12 @@ const App = () => {
           secureTextEntry
           placeholder='senha123'
           onChangeText={(text) => {
-            password = text;
+            password.current = text;
           }}
         />
       </View>
       {!!error && <Text style={{ color: 'red' }}>{error}</Text>}
-      <Button
-        onPress={() => {
-          if (email == '' || password == '') {
-            setError('Preencha todos os campos');
-          } else if (password.length < 7) {
-            setError('A senha deve possuir pelo menos 7 caracteres');
-          } else if (!emailRegex.test(email)) {
-            setError('O campo de email deve seguir o formato de um email: abc@def.com');
-          } else if (!passNumberRegex.test(password) || !passCharRegex.test(password)) {
-            setError('O campo de senha deve conter ao menos uma letra e um número');
-          } else {
-            setError('');
-          }
-        }}
-        title='Login'
-      />
+      <Button onPress={handleButtonPress} title='Login' />
     </SafeAreaView>
   );
 };
