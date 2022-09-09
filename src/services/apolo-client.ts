724c45ae6ddba04c91a501855d4b3707452294da
japoncio3k <gustavo.kamihara@gmail.com>
2022-09-09 13:27:27 -0300

    Formated .ts files

--- src/services/apolo-client.ts
+++ src/services/apolo-client.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
--- src/utils/login-validator.ts
+++ src/utils/login-validator.ts
@@ -1,15 +1,15 @@
 import { passCharRegex, passNumberRegex, emailRegex } from './regex';
 
-export const validateLogin = (email:string,password:string)=>{
-    let error:string | null = null
-    if (email == '' || password == '') {
-        error = 'Preencha todos os campos'
-      } else if (password.length < 7) {
-        error = 'A senha deve possuir pelo menos 7 caracteres'
-      } else if (!emailRegex.test(email)) {
-        error = 'O campo de email deve seguir o formato de um email: abc@def.com'
-      } else if (!passNumberRegex.test(password) || !passCharRegex.test(password)) {
-        error = 'O campo de senha deve conter ao menos uma letra e um número'
-      }
-    return error;
-}
+export const validateLogin = (email: string, password: string) => {
+  let error: string | null = null;
+  if (email == '' || password == '') {
+    error = 'Preencha todos os campos';
+  } else if (password.length < 7) {
+    error = 'A senha deve possuir pelo menos 7 caracteres';
+  } else if (!emailRegex.test(email)) {
+    error = 'O campo de email deve seguir o formato de um email: abc@def.com';
+  } else if (!passNumberRegex.test(password) || !passCharRegex.test(password)) {
+    error = 'O campo de senha deve conter ao menos uma letra e um número';
+  }
+  return error;
+};
